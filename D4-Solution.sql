2f8bc03d303b9b84e130f1c1592bf55711ceeef0
MengtingSong <mtsong@bu.edu>
2021-09-26 01:47:35 -0700

    D4-completed

--- D4-Solution.sql
+++ D4-Solution.sql
@@ -1,3 +1,7 @@
+-- 1
+USE [D4-Q1]
+GO
+
 CREATE TABLE HeadOffices
 (
     id INT PRIMARY KEY IDENTITY(1,1),
@@ -5,9 +9,7 @@ CREATE TABLE HeadOffices
     address VARCHAR(100),
     phone_number VARCHAR(20) NOT NULL,
     director VARCHAR(40),
-    city_id INT NOT NULL,
-    CONSTRAINT fk_cities FOREIGN KEY (city_id)
-    REFERENCES Cities(id),
+    city_id INT NOT NULL
 );
 
 CREATE TABLE Projects
@@ -17,10 +19,9 @@ CREATE TABLE Projects
     project_title VARCHAR(40),
     starting_date DATETIME,
     end_date DATETIME,
-    budget FLOAT,
+    budget FLOAT CHECK(budget >= 0),
     person_in_charge VARCHAR(40),
-    office_id INT NOT NULL
-    FOREIGN KEY Headoffices(id)
+    office_id INT NOT NULL FOREIGN KEY REFERENCES HeadOffices(id)
 );
 
 CREATE TABLE Cities
@@ -28,15 +29,131 @@ CREATE TABLE Cities
     id INT PRIMARY KEY IDENTITY(1,1),
     city VARCHAR(20) NOT NULL,
     country VARCHAR(20) NOT NULL,
-    inhabitant_number INT
+    inhabitant_number INT CHECK(inhabitant_number >= 0)
 );
 
 CREATE TABLE ProjectsInCities
 (
     project_id INT NOT NULL
-    FOREIGN KEY REFERENCES Projects.id,
+    FOREIGN KEY REFERENCES Projects(id),
     city_id INT NOT NULL
-    FOREIGN KEY REFERENCES Cities.id,
+    FOREIGN KEY REFERENCES Cities(id),
     CONSTRAINT
     pk PRIMARY KEY(project_id, city_id)
 );
+
+ALTER TABLE HeadOffices
+ADD CONSTRAINT fk_cities 
+FOREIGN KEY (city_id) REFERENCES Cities(id)
+
+-- 2
+USE [D4-Q2]
+GO
+
+CREATE TABLE lenders
+(
+    id INT PRIMARY KEY IDENTITY(1,1),
+    name VARCHAR(40) NOT NULL,
+    amount_of_money FLOAT NOT NULL CHECK(amount_of_money >= 0)
+);
+GO
+
+CREATE TABLE borrowers
+(
+    id INT PRIMARY KEY IDENTITY(1,1),
+    name VARCHAR(40) NOT NULL,
+    risk INT
+);
+GO
+
+CREATE TABLE loans
+(
+    code INT PRIMARY KEY IDENTITY(1,1),
+    amount FLOAT NOT NULL CHECK(amount >= 0),
+    refund_ddl DATETIME NOT NULL,
+    interest FLOAT NOT NULL CHECK(interest >= 0),
+    purpose VARCHAR(100),
+    borrower_id INT NOT NULL FOREIGN KEY REFERENCES borrowers(id)
+)
+GO
+
+CREATE TABLE loans_under_lenders
+(
+    lenders_id INT NOT NULL FOREIGN KEY REFERENCES lenders(id),
+    loans_code INT NOT NULL FOREIGN KEY REFERENCES loans(code)
+    CONSTRAINT com_pk
+    PRIMARY KEY (lenders_id, loans_code)
+);
+GO
+
+-- 3
+USE [D4-Q3]
+GO
+
+CREATE TABLE courses
+(
+    id INT PRIMARY KEY IDENTITY(1,1),
+    name VARCHAR(20) UNIQUE NOT NULL,
+    description VARCHAR(100) NOT NULL,
+    photo VARCHAR(100),
+    price FLOAT NOT NULL CHECK(price > 0),
+    employee_id INT NOT NULL
+);
+GO
+
+CREATE TABLE categories
+(
+    id INT PRIMARY KEY IDENTITY(1,1),
+    name VARCHAR(20) UNIQUE NOT NULL 
+);
+GO
+
+CREATE TABLE employees
+(
+    id INT PRIMARY KEY IDENTITY(1,1),
+    name VARCHAR(40) NOT NULL
+);
+GO
+
+ALTER TABLE courses
+ADD CONSTRAINT emp_fk FOREIGN KEY (employee_id) REFERENCES employees(id)
+GO
+
+CREATE TABLE course_category
+(
+    category_id INT NOT NULL,
+    course_id INT NOT NULL,
+    CONSTRAINT composite_pk
+    PRIMARY KEY (category_id, course_id)
+);
+GO
+
+CREATE TABLE employee_category
+(
+    employee_id INT NOT NULL,
+    category_id INT NOT NULL,
+    CONSTRAINT composite_pk
+    PRIMARY KEY (employee_id, category_id)
+);
+GO
+
+CREATE TABLE ingredients
+(
+    id INT PRIMARY KEY IDENTITY(1,1),
+    name VARCHAR(20) UNIQUE NOT NULL,
+    units VARCHAR(20) NOT NULL,
+    current_amount INT NOT NULL CHECK(current_amount >= 0)
+);
+GO
+
+CREATE TABLE recipies
+(
+    course_id INT NOT NULL,
+    ingredient_id INT NOT NULL,
+    required_amount INT NOT NULL CHECK(required_amount >= 0),
+    CONSTRAINT composite_pk
+    PRIMARY KEY(course_id, ingredient_id)
+);
+GO
+
+
